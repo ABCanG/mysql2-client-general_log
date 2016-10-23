b06c93a71e6060dc6e2d56c0a12af435c2071b92
ABCanG <abcang1015@gmail.com>
2016-10-23 15:17:47 +0900

    add example

---
+++ example/test.rb
@@ -0,0 +1,49 @@
+require 'sinatra'
+require 'mysql2'
+require "mysql2/client/general_log"
+
+
+helpers do
+  def db
+    Thread.current[:db] ||= Mysql2::Client.new(
+      host: "127.0.0.1",
+      username: "root",
+    )
+  end
+
+  def init
+    db.query("DROP DATABASE IF EXISTS `mysql2_client_general_log_test`")
+    db.query("CREATE DATABASE `mysql2_client_general_log_test`")
+    db.query("USE `mysql2_client_general_log_test`")
+    db.query(<<-SQL)
+    CREATE TABLE users (
+      `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      `name` varchar(255) NOT NULL UNIQUE,
+      `password` varchar(255) NOT NULL
+    );
+    SQL
+    db.query(<<-SQL)
+    INSERT INTO `users` (`name`, `password`)
+    VALUES ('ksss', 'cheap-pass'),
+    ('foo', 'fooo'),
+    ('bar', 'barr')
+    ;
+    SQL
+  end
+end
+
+get '/' do
+  db.query("USE `mysql2_client_general_log_test`")
+  db.query("SELECT * FROM users WHERE name = '#{"ksss"}'")
+  stmt = db.prepare('SELECT * FROM users WHERE name = ?')
+  stmt.execute('barr')
+  stmt.execute('foo')
+end
+
+get '/init' do
+  init
+end
+
+after do
+  db.general_log.writefile(req: request, backtrace: true)
+end
